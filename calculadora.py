8faf9e50feeb20e5dcbe26e1cc472f52b0199136
matheusallein <mtsallein@gmail.com>
2021-11-30 18:36:51 -0300

    Closed #5 - funcionalidade de multiplicação

--- calculadora.py
+++ calculadora.py
@@ -3,8 +3,22 @@
 # TODO: Adicionar funções com as 4 operações
 
 
+def multiplica(a, b):
+    """Retorna o resultado da multiplicação de dois números
+
+    Args:
+        a (int): número inteiro
+        b (int): número inteiro
+
+    Returns:
+        int: Resultado da multiplicação de "a" e "b"
+    """
+    return int(a) * int(b)
+
+
 if __name__ == "__main__":
 
     print("---------- Caluladora Hardcoded ----------")
 
     # TODO: Escrever exemplos usando as funçoes.
+    print(multiplica(2, 2))
