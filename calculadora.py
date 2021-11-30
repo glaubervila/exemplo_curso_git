468122ffe77df070a9327c6ba3e876db4a105106
glaubervila <glauber.vila.verde@gmail.com>
2021-11-30 16:17:50 -0300

    Adicionado arquivo calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# TODO: Adicionar funções com as 4 operações
+
+
+if __name__ == "__main__":
+
+    print("---------- Caluladora Hardcoded ----------")
+
+    # TODO: Escrever exemplos usando as funçoes.
