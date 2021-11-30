c7d7f2c5db519bf810b448e1d8e9618cd1d75ddb
Fernando Silva <fernando_nicolau@outlook.com>
2021-11-30 18:46:57 -0300

    Closed #3 - Foi adicionado uma função que executa a subtração entre dois valores inteiros.

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+def subtracao(a, b):
+    """Retorna a subtração entre 2 valores inteiros
+    Args:
+        a (int): qualquer valor inteiro
+        b (int): qualquer valor inteiro
+    Returns:
+        int: Resultado da subtração entre a e b
+    """
+    return int(a) - int(b)
+
+
+if __name__ == "__main__":
+
+    print("---------- Caluladora Hardcoded ----------")
+
+    # TODO: Escrever exemplos usando as funçoes.
+    print(subtracao(2, 2))
